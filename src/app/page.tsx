f21cbbc978cdd20a09bddb74ebd7a7dc911bbc6a
Chris Ware <c.p.ware@hotmail.com>
2023-12-01 16:01:04 -0700

    Post Hero Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "@/components/Hero/Hero";
 import PostHero from "@/components/PostHero/PostHero";
+import Featured from "@/components/Featured/Featured";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <PostHero />
+      <Featured />
     </div>
   );
 }
---
+++ src/components/Featured/Featured.tsx
@@ -0,0 +1,14 @@
+import ContentPadding from "../ContentPadding/ContentPadding";
+import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
+import styles from "./Featured.module.css";
+
+const Featured = () => {
+  return (
+    <LayoutWrapper>
+      <ContentPadding>
+        <h2>Featured properties</h2>
+      </ContentPadding>
+    </LayoutWrapper>
+  );
+};
+export default Featured;
