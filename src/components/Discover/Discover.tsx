44213837592764f56b81f5ab078cb7d2c3132f7c
Chris Ware <c.p.ware@hotmail.com>
2023-12-03 12:09:23 -0700

    Discover Done

--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -1,12 +1,31 @@
 import ContentPadding from "../ContentPadding/ContentPadding";
 import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./Discover.module.css";
+import Image from "next/image";
+import Img from "../../../public/images/img1.jpg";
+import Button from "../Button/Button";
 
 const Discover = () => {
   return (
     <LayoutWrapper>
       <ContentPadding>
-        <h2>Discover</h2>
+        <div className={styles.container}>
+          <Image src={Img} alt='img' fill className={styles.img} />
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <h3 className={styles.heading}>
+                <span className={styles.highlight}>Discover properties</span>
+                <br /> that will improve your life and that of your family
+              </h3>
+              <p className={styles.copy}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+                nibh pretium nunc mauris sed adipiscing.
+              </p>
+              <Button btnType='navBtn' text='To catelog' href='/' />
+            </div>
+            <div className={styles.right}></div>
+          </div>
+        </div>
       </ContentPadding>
     </LayoutWrapper>
   );
