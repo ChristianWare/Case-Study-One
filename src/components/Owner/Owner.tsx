0d5356024e18a5140998ba81e2359d1a4ac6717e
Chris Ware <c.p.ware@hotmail.com>
2023-12-03 20:35:49 -0700

    Owner Done

--- src/components/Owner/Owner.tsx
+++ src/components/Owner/Owner.tsx
@@ -3,7 +3,7 @@ import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./Owner.module.css";
 import Image from "next/image";
 import Img from "../../../public/images/img1.jpg";
-import Quote from '../../../public/icons/quote.svg'
+import Quote from "../../../public/icons/quote.svg";
 
 const Owner = () => {
   return (
@@ -11,7 +11,7 @@ const Owner = () => {
       <LayoutWrapper>
         <ContentPadding>
           <div className={styles.top}>
-                <Quote width={130} height={130} className={styles.quote} />
+            <Quote width={130} height={130} className={styles.quote} />
           </div>
           <div className={styles.content}>
             <div className={styles.left}>
@@ -28,6 +28,21 @@ const Owner = () => {
             <div className={styles.right}>
               <div className={styles.imgContainer}>
                 <Image src={Img} alt='image' fill className={styles.img} />
+              </div>
+              <div className={styles.statsContainer}>
+                <div className={styles.statsLeft}>
+                  <h4 className={styles.statsHeading}>Kristin Watson</h4>
+                  <p className={styles.statsTitle}>Product manager at Joon</p>
+                </div>
+                <div className={styles.statsRight}>
+                  <Image
+                    src={Img}
+                    alt='image'
+                    width={80}
+                    height={80}
+                    className={styles.imgii}
+                  />
+                </div>
               </div>
             </div>
           </div>
