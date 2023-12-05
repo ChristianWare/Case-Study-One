b341153e5bd7ba16c565462015774b8714da1443
Chris Ware <c.p.ware@hotmail.com>
2023-12-05 13:46:02 -0700

    Footer Done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,103 @@
+"use client";
+
+import { footer } from "@/lib/data";
 import ContentPadding from "../ContentPadding/ContentPadding";
 import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./Footer.module.css";
+import Link from "next/link";
+import { useState } from "react";
+import ArrowRight from "../../../public/icons/arrowRight.svg";
+import House from "../../../public/icons/house.svg";
 
 const Footer = () => {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i: any) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
+
   return (
-    <LayoutWrapper>
-      <ContentPadding>
-        <h2>Footer Here</h2>
-      </ContentPadding>
-    </LayoutWrapper>
+    <>
+      {/* <ContentPadding> */}
+      <footer className={styles.contanier}>
+        <div className={styles.content}>
+          <Link href='/' className={styles.logo}>
+            <House width={30} height={30} className={styles.icon} /> Homix
+          </Link>{" "}
+          <div className={styles.middleDesktop}>
+            {footer.map((f, index) => (
+              <div key={index} className={styles.sectionContainer}>
+                <div className={styles.headingContainer}>
+                  <p className={styles.heading}>{f.heading}</p>
+                </div>
+                <div className={styles.footerItemContainer}>
+                  {f.section.map((s, index) => (
+                    <p key={index} className={styles.footerItem}>
+                      {s.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={styles.middleMobile}>
+            {footer.map((f, i) => (
+              <div key={f.id} className={styles.sectionContainer}>
+                <div
+                  className={styles.headingContainer}
+                  onClick={() => toggle(i)}
+                >
+                  <p className={styles.heading}>{f.heading}</p>
+                  <span className={styles.arrow}>
+                    {selected === i ? (
+                      <ArrowRight
+                        className={styles.arrowFlip}
+                        width={20}
+                        height={20}
+                      />
+                    ) : (
+                      <ArrowRight
+                        width={20}
+                        height={20}
+                        className={styles.arrow}
+                      />
+                    )}
+                  </span>
+                </div>
+                <div
+                  className={
+                    selected === i
+                      ? styles.answer + " " + styles.show
+                      : styles.answer
+                  }
+                >
+                  {f.section.map((s, index) => (
+                    <p key={index} className={styles.footerItem}>
+                      {s.name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.bottomLeft}>
+              <p className={styles.copy}>
+                &copy; 2023 Golf guise || All Rights Reserved{" "}
+              </p>
+            </div>
+            <div className={styles.bottomRight}>
+              {/* <Socials borderSize='smallBorder' iconSize='smallIcon' /> */}
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* </ContentPadding> */}
+    </>
   );
 };
 export default Footer;
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -24,3 +24,96 @@ export const faqs = [
       "We implement SEO strategies to improve the visibility of your website on search engines. Additionally, we can assist with marketing strategies to drive traffic to your platform.",
   },
 ] as const;
+
+const footer = [
+  {
+    id: 1,
+    heading: "About",
+    section: [
+      {
+        id: 2,
+        name: "Terms of Use",
+      },
+      {
+        id: 3,
+        name: "Privacy Poilicy",
+      },
+      {
+        id: 4,
+        name: "Accessibility Policy",
+      },
+      {
+        id: 5,
+        name: "California Disclosures",
+      },
+    ],
+  },
+  {
+    id: 6,
+    heading: "Reviews",
+    section: [
+      {
+        id: 7,
+        name: "Best Clubs",
+      },
+      {
+        id: 8,
+        name: "Drivers",
+      },
+      {
+        id: 9,
+        name: "Putters",
+      },
+      {
+        id: 10,
+        name: "Hybrids",
+      },
+    ],
+  },
+  {
+    id: 11,
+    heading: "Courses",
+    section: [
+      {
+        id: 12,
+        name: "United States",
+      },
+      {
+        id: 13,
+        name: "Europe",
+      },
+      {
+        id: 14,
+        name: "Canada",
+      },
+      {
+        id: 15,
+        name: "Asia",
+      },
+    ],
+  },
+  {
+    id: 16,
+    heading: "Tours",
+    section: [
+      {
+        id: 17,
+        name: "PGA",
+      },
+      {
+        id: 18,
+        name: "U.S. Open",
+      },
+      {
+        id: 19,
+        name: "Masters",
+      },
+      {
+        id: 20,
+        name: "Ryder Cup",
+      },
+    ],
+  },
+];
+
+export { footer };
