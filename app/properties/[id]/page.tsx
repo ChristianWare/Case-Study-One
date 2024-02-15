f23ee7518451900253aa44a4ca1a8667bf672901
Chris Ware <c.p.ware@hotmail.com>
2024-02-15 15:37:02 -0700

    reval iii

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -5,10 +5,11 @@ interface Props {
   params: { id: string };
 }
 
-export const dynamic = "force-static";
 
 const getRoom = async (id: string) => {
-  const res = await fetch(`${process.env.API_URI}/api/rooms/${id}`);
+  const res = await fetch(`${process.env.API_URI}/api/rooms/${id}`, {
+    cache: "no-cache",
+  });
   return res.json();
 };
 
