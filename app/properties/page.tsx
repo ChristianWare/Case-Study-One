7dfe4a368b9de8465e5024e746c9d8a573ccc55e
Chris Ware <c.p.ware@hotmail.com>
2024-04-11 21:21:12 -0700

    test

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -6,14 +6,16 @@ import FinalCTA2 from "../../components/FinalCTA2/FinalCTA2";
 import PageIntro from "../../components/PageIntro/PageIntro";
 import Error from "../error";
 import Properties from "../../components/Properties/Properties";
+import { revalidatePath } from "next/cache";
 
 // export const revalidate = 0;
-// export const dynamic = "force-static";
+export const dynamic = "force-static";
 
 const getRooms = async () => {
   try {
     const res = await fetch(`${process.env.API_URL}/api/rooms`);
     const data = res.json();
+    revalidatePath("/api/rooms");
     return data;
   } catch (error) {
     console.log("error => ", error);
