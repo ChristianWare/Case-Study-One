7927cf26b4990367ce2f258d00e9ed64f739e51b
Chris Ware <c.p.ware@hotmail.com>
2024-04-12 21:37:35 -0700

    UI  User settings pageupdate

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import LayoutWrapper from "../../components/LayoutWrapper/LayoutWrapper";
 import ContentPadding from "../../components/ContentPadding/ContentPadding";
 import FinalCTA1 from "../../components/FinalCTA1/FinalCTA1";
+import styles from "./settingStyles.module.css";
 
 interface Props {
   children: ReactNode;
@@ -10,25 +11,21 @@ interface Props {
 
 const UserLayout = ({ children }: Props) => {
   return (
-    <>
-      <LayoutWrapper>
-        <ContentPadding>
-          <div className='mt-2 mb-4 bg-light py-4'>
-            <h1 className='text-secondary text-center'>User Settings</h1>
-          </div>
+    <LayoutWrapper>
+      <ContentPadding>
+        <div className='mt-2 mb-4 bg-light py-4'>
+          <h1 className='text-secondary text-center'>User Settings</h1>
+        </div>
 
-          <div className='container'>
-            <div className='row justify-content-around'>
-              <div className='col-12 col-lg-3'>
-                <UserSidebar />
-              </div>
-              <div className='col-12 col-lg-8 user-dashboard'>{children}</div>
-            </div>
+        <div className={styles.bottom}>
+          <div className={styles.left}>
+            <UserSidebar />
           </div>
-      <FinalCTA1 />
-        </ContentPadding>
-      </LayoutWrapper>
-    </>
+          <div className={styles.right}>{children}</div>
+        </div>
+        <FinalCTA1 />
+      </ContentPadding>
+    </LayoutWrapper>
   );
 };
 export default UserLayout;
