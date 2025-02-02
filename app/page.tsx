1db90ac15f1009770a327539960bd8a49fbcedf7
Chris Ware <c.p.ware@hotmail.com>
2025-02-02 01:51:06 -0700

    svg clip mask

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default async function Home() {
     <div>
       <Hero />
       <Welcome />
-      {/* <AboutSectionii /> */}
+      <AboutSectionii />
       {/* <AboutSectionii />
       <Amenities />
       <Amenitiesii />
